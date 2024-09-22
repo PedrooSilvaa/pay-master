e482e7553d49663575fd3568194f2c5e14a691b3
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-22 18:36:47 -0300

    feat: create User entity

---
+++ src/main/java/tech/silva/payment/entity/User.java
@@ -0,0 +1,24 @@
+package tech.silva.payment.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String cpf;
+
+    private float value;
+}
