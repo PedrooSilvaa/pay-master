0327b8ba0c7bffc9e2addc9f26902e737f1358b2
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-22 18:30:59 -0300

    feat: create project

---
+++ src/main/java/tech/silva/payment/PayMasterApplication.java
@@ -0,0 +1,13 @@
+package tech.silva.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/silva/payment/PayMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.silva.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
